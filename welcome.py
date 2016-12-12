b5b0b5bcfa360513f91a0db9a33d888fecbaa4c2
Mohannad Alahmadi <mohairish@gmail.com>
2016-12-12 07:40:42 -0600

    uploaded the gmap example with navbar and chart page

--- welcome.py
+++ welcome.py
@@ -1,42 +1,52 @@
-# Copyright 2015 IBM Corp. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+'''
+A Web application that shows Google Maps around schools, using
+the Flask framework, and the Google Maps API.
+'''
 
-import os
-from flask import Flask, jsonify, render_template, abort
-import gmplot
-
+from flask import Flask, render_template, abort
 app = Flask(__name__)
+
+
+class School:
+    def __init__(self, key, name, lat, lng):
+        self.key  = key
+        self.name = name
+        self.lat  = lat
+        self.lng  = lng
+
+schools = (
+    School('hv',      'Happy Valley Elementary',   37.9045286, -122.1445772),
+    School('stanley', 'Stanley Middle',            37.8884474, -122.1155922),
+    School('wci',     'Walnut Creek Intermediate', 37.9093673, -122.0580063)
+)
+schools_by_key = {school.key: school for school in schools}
+
+
+@app.route("/index")
+def mainpage():
+    return render_template('index.html', schools=schools)
 
 @app.route("/")
 def index():
+    return render_template('index.html', schools=schools)
+
+@app.route("/<school_code>")
+def show_school(school_code):
+    school = schools_by_key.get(school_code)
+    if school:
+        return render_template('map.html', school=school)
+    else:
+        abort(404)
 
 
-    gmap = gmplot.GoogleMapPlotter(22.060191, 39.216372, 14)
-    lats = [22.137180, 22.079267, 22.060191, 22.053628, 22.137240, 22.079127, 22.040121, 22.053478, 22.133280,
-            22.0796467, 22.060591, 22.058728, 22.137540, 22.077527, 22.045321, 22.064478]
-    lngs = [39.186157, 39.194130, 39.216372, 39.224843, 39.186657, 39.194820, 39.216752, 39.224342, 39.186157,
-            39.194130, 39.216372, 39.224843, 39.186657, 39.194820, 39.216752, 39.224342]
-    # gmap.plot(lats, lngs, 'cornflowerblue', edge_width=10)
-    # gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
-    # gmap.scatter(marker_lats, marker_lngs, 'k', marker=True)
-    gmap.heatmap(lats, lngs)
-
-    #gmap.draw("templates/index.html")
-    return render_template('index.html')
+@app.route("/graph_example")
+def graph(chartID = 'chart_ID', chart_type = 'bar', chart_height = 350):
+	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+	series = [{"name": 'Label1', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+	title = {"text": 'My Title'}
+	xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+	yAxis = {"title": {"text": 'yAxis Label'}}
+	return render_template('index.html', pageType='graph',chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
 
-
-port = os.getenv('PORT', '5001')
-if __name__ == "__main__":
-	app.run(host='0.0.0.0',port=port)
+app.run(debug=True)
