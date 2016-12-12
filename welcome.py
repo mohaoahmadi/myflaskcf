40a49ec28fe44f7a04dd82c494f600ab152da1f9
Mohannad Alahmadi <mohairish@gmail.com>
2016-12-12 07:50:46 -0600

    added required modules. sorry for copying and pasting :/

--- welcome.py
+++ welcome.py
@@ -1,12 +1,22 @@
-'''
-A Web application that shows Google Maps around schools, using
-the Flask framework, and the Google Maps API.
-'''
+# Copyright 2015 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from flask import Flask, render_template, abort
+import os
+from flask import Flask, jsonify, render_template, abort
+import gmplot
+
 app = Flask(__name__)
-
-
 class School:
     def __init__(self, key, name, lat, lng):
         self.key  = key
